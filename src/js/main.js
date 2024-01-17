bf8316f2775270b1ef6639c7af452e916c6a8e44
Ilya <illyaetoilysha@gmail.com>
2024-01-17 22:48:19 +0200

    4.3

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,10 @@
-const cartButton = document.querySelector('#cart-button')
+'use strict'
 
+const cartButton = document.querySelector('#cart-button')
 const modal = document.querySelector('.modal')
 const close = document.querySelector('.close')
 const body = document.querySelector('body')
-
-cartButton.addEventListener('click', toggleModal)
-
-
-function toggleModal() {
-  close.removeEventListener('click', toggleModal)
-  close.addEventListener('click', toggleModal)
-  modal.classList.toggle('hidden')
-  body.classList.toggle('overflow-hidden')
-}
-
+const logo = document.querySelector('.logo')
 
 const authButton = document.querySelector('.auth-button')
 const authModal = document.querySelector('.auth-modal')
@@ -25,8 +16,22 @@ const logoutButton = document.querySelector('.logout-button')
 const authModalInner = document.querySelector('.modal-inner')
 const authModalBackdrop = document.querySelector('.modal-backdrop')
 
+const cardsRestaurants = document.querySelector('.cards-restaurants')
+const cardsMenu = document.querySelector('.menu')
+const menuBlock = document.querySelector('.menu-block')
+const main = document.querySelector('.main')
+
+cartButton.addEventListener('click', toggleModal)
+
+function toggleModal() {
+  close.removeEventListener('click', toggleModal)
+  close.addEventListener('click', toggleModal)
+  modal.classList.toggle('hidden')
+  body.classList.toggle('overflow-hidden')
+}
+
 authModalBackdrop.addEventListener('click', toggleModalAuth)
-authModalInner.addEventListener('click', (event) => event.stopPropagation())
+authModalInner.addEventListener('click', event => event.stopPropagation())
 let login = localStorage.getItem('user')
 
 function toggleModalAuth() {
@@ -79,7 +84,6 @@ function notAuthorized() {
     loginForm.removeEventListener('submit', logIn)
     loginForm.reset()
     checkAuth()
-
   }
 
   authButton.addEventListener('click', toggleModalAuth)
@@ -97,3 +101,106 @@ function checkAuth() {
 }
 
 checkAuth()
+
+function createCardRestaurant() {
+  const card = document.createElement('a')
+  //{ image, kitchen, name, price, stars, products, time_of_delivery: timeOfDelivery }
+  // card.className = 'card card-restaurant'
+  // card.products = products
+  // card.info = [name, price, stars, kitchen]
+
+  card.insertAdjacentHTML(
+    'beforeend',
+    `
+    <div class='rounded-lg shadow-md overflow-hidden reveal-step-animation card'>
+      <div
+        class='w-full bg-cover bg-center bg-no-repeat h-[250px] bg-[url(/src/images/products/pizza-plus.jpg)]'
+      ></div>
+      <div class='bg-white md:px-6 sm:px-5 px-4 sm:pt-5 pt-4 md:pb-9 sm:pb-8 pb-7'>
+        <div class='flex items-center justify-between'>
+          <h4 class='font-bold md:text-2xl sm:text-xl text-lg'>Пицца плюс</h4>
+
+          <div class='px-2 bg-black text-white sm:text-[12px] text-[11px] leading-5 rounded-sm'>50 мин</div>
+        </div>
+
+        <div class='flex items-baseline space-x-6 mt-2.5'>
+          <div class='text-yellow-rating flex items-baseline sm:text-base text-sm'>
+            <img src='./src/images/icons/rating.svg' alt='search icon' class='mr-2' />
+
+            4.5
+          </div>
+
+          <div class='text-gray-7 md:text-lg sm:text-base text-sm'>
+            От 900 ₴
+            <span class='font-bold px-2'>•</span> Пицца
+          </div>
+        </div>
+      </div>
+    </div>
+	`
+  )
+  console.log(card)
+  cardsRestaurants.insertAdjacentElement('beforeend', card)
+}
+createCardRestaurant()
+createCardRestaurant()
+createCardRestaurant()
+
+cardsRestaurants.addEventListener('click', openGoods)
+
+function openGoods(event) {
+  const target = event.target
+  const restaurant = target.closest('.card')
+
+  if (login) {
+    if (restaurant) {
+      menuBlock.classList.remove('hidden')
+      main.classList.add('hidden')
+
+      cardsMenu.textContent = ''
+
+      createCardGood()
+      createCardGood()
+      createCardGood()
+    }
+  } else {
+    toggleModalAuth()
+  }
+}
+
+function createCardGood() {
+  //{ description, image, name, price, id }
+  const card = document.createElement('div')
+  card.className = 'rounded-lg shadow-md overflow-hidden'
+
+  card.insertAdjacentHTML(
+    'beforeend',
+    `
+      <div class="w-full bg-cover bg-center bg-no-repeat h-[250px] bg-[url(/src/images/products/sushi/sushi1.png)]"></div>
+      <div class="bg-white md:px-6 sm:px-5 px-4 sm:pt-5 pt-4 md:pb-9 sm:pb-8 pb-7">
+        <div class="space-y-2.5">
+          <h4 class="md:text-2xl sm:text-xl text-lg line-clamp-1">Ролл угорь стандарт</h4>
+
+          <p class="text-gray-7 line-clamp-2 sm:text-base text-sm">
+            Рис, угорь, соус унаги, кунжут, водоросли нори.
+          </p>
+        </div>
+
+        <div class="space-x-8 mt-6 flex items-center">
+          <a href="#" class="py-2 px-4 borderrounded-sm text-white bg-blue-6 flex items-center">
+            <span>В корзину</span>
+            <img src="./src/images/icons/cart-white.svg" alt="cart icon" class="ml-1" />
+          </a>
+          <div class="sm:text-xl font-bold text-lg">250 ₴</div>
+        </div>
+      </div>
+	`
+  )
+
+  cardsMenu.insertAdjacentElement('beforeend', card)
+}
+
+logo.addEventListener('click', () => {
+  menuBlock.classList.add('hidden')
+  main.classList.remove('hidden')
+})
